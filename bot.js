a3054ce17c9143ada6bc70cfe409e91ed9d8796d
Jackie Keiser <jackie.keiser@wdgie.com>
2016-12-18 13:30:48 -0600

    added mythic command. closes #6

--- bot.js
+++ bot.js
@@ -146,6 +146,31 @@ bot.on("message", msg => {
     });
   }
 
+  if (command === "mythic") {
+    request("http://www.wowhead.com", function(error, response, body){
+      if (error || response.statusCode != 200) {
+        throw new Error("Couldn't access mythic+ affixes.");
+        return;
+      }
+      const $ = cheerio.load(body);
+      var embed = {
+        fields: [],
+      };
+
+      $('.tiw-region-US .tiw-group-mythicaffix tr .icon-both').each(function(i){
+        var name = $(this).find('a').text();
+        var url = 'http://www.wowhead.com'+$(this).find('a').attr('href');
+
+        embed.fields.push({
+          name: name,
+          value: url
+        });
+      });
+
+      msg.channel.sendMessage('', { embed });
+    });
+  }
+
   if (command === "emissary" || command === "emmisary" || command === "emmissary" ) {
     request("http://www.wowhead.com", function(error, response, body){
       if (error || response.statusCode != 200) {
