2ddcac9e2b269e1e6c9e910d4d9ac8bddf56bf79
Jackie Keiser <jackie.keiser@wdgie.com>
2018-07-28 09:38:10 -0500

    adding role command

---
+++ commands/roles.js
@@ -0,0 +1,23 @@
+const presence = require('./helpers/presence');
+
+exports.run = function (bot, msg, args = []) {
+  if (msg.author.username != 'MongooseDoom') { return; }
+
+  let guild = bot.guilds.first();
+
+  // Update member presence on ready
+  guild.members.forEach(function (member) {
+    presence.update(member);
+  });
+};
+
+exports.conf = {
+  enabled: true, // not used yet
+  aliases: [],
+};
+
+exports.help = {
+  name: 'roles',
+  description: 'Fixes channel rolls',
+  usage: 'roles'
+};
