64e8c2914d73933bebc45b3cd5192a1fec1e03b8
Jackie Keiser <jackie.keiser@wdgie.com>
2019-04-20 15:36:20 -0500

    removing api token from config.json

--- helpers/toon.js
+++ helpers/toon.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const get = function (character, realm, fields){
   return new Promise((resolve, reject) => {
-    request(`https://us.api.battle.net/wow/character/${realm}/${character}?fields=${fields}&locale=en_US&apikey=${config.api}`, function(error, response, body){
+    request(`https://us.api.battle.net/wow/character/${realm}/${character}?fields=${fields}&locale=en_US&apikey=${process.env.BATTLENET_TOKEN}`, function(error, response, body){
       if (error) { reject(error); }
       if (response.statusCode !== 200) { reject(new Error('Expected statusCode === 200')); }
       try {
